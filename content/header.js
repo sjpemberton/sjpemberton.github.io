323e23b5ab8c7180a2bf5b16abcb7d50bdf90e88
Steve Pemberton <sjpemberton09@gmail.com>
2015-02-12 00:58:54 +0000

    CSS overhaul and about page.

--- content/header.js
+++ content/header.js
@@ -1,7 +1,8 @@
 window.onscroll = function(e) {
 	var header = document.getElementById('header');
+	var scrollTarget = document.getElementById('home-header').offsetHeight;
 	var content = document.getElementById('page-content');
-	if (this.scrollY >= 200){  
+	if (this.scrollY >= scrollTarget){  
 		if(header.className.indexOf('sticky') == -1){
 	     	content.className = header.className = header.className + " sticky";
 			}
