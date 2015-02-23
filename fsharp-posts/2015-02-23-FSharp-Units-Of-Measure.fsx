9d4dacaeee8d6655037871fe23e4bed7bee99c72
Steve Pemberton <sjpemberton09@gmail.com>
2015-02-23 23:31:02 +0000

    UoM post draft

---
+++ fsharp-posts/2015-02-23-FSharp-Units-Of-Measure.fsx
@@ -0,0 +1,22 @@
+(*** raw ***)
+---
+layout: post
+title: F# Units of Measure - A Working Example
+date: 23/02/2015
+tags: ["fsharp"]
+meta: Utilising FSharp Units of Measure for type safe calculations
+---
+
+(**
+
+#F# Units of Measure
+
+Have you ever wished that you could have type safe calculations throughout your application?  
+Or have you wasted time tracking down a bug due to somebody using the wrong unit in a calculation?
+
+If so then `FSharp Unit of Measure` (UoM) will be right up your street.
+
+
+Units of Measure 
+
+*)
