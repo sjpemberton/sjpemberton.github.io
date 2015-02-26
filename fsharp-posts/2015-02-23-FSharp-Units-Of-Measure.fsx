660fdf9d21b42f6fbe5b941032fddc1e61ab2553
Steve Pemberton <sjpemberton09@gmail.com>
2015-02-26 23:32:12 +0000

    UoM post update

--- fsharp-posts/2015-02-23-FSharp-Units-Of-Measure.fsx
+++ fsharp-posts/2015-02-23-FSharp-Units-Of-Measure.fsx
@@ -1,7 +1,7 @@
 (*** raw ***)
 ---
 layout: post
-title: F# Units of Measure - A Working Example
+title: F# Units of Measure - A Worked Example
 date: 23/02/2015
 comments: true
 tags: ["fsharp"]
@@ -21,6 +21,8 @@ In this post I will explore the various ways of using `F# Units of Measure` and 
 I decided I would put `UoM` to the test by using them in a real world example and what could be a better application then that of brewing beer.  
 Therefore the following is a worked example of various calculations used at various stages of the brewing process.
 
+<!-- more -->
+
 ##Units of Measure - An introduction
 
 Units of measure in F# are types that can be associated with floating point or signed integer values.  
@@ -30,6 +32,11 @@ For example, we can declare a `UoM` for some of the measures of volume we will n
 
 *)
 
+
+(*** hide ***)
+    [<Measure>] type gp
+
+(** *)
     ///Litre (or Liter in the US)
     [<Measure>] type L
 
@@ -43,22 +50,60 @@ Once we have these measures defined we can use them in a number of ways
 - To return a specific unit of measure from a function
 - For defining new units of measure in terms of the original
 
-Here is an example of a function that can only take a value that is in `usGal`
+Here is an example of a function that can only take a values that have a dimension specified.
 
 *)
 
-    ///Converts a potential extract value (PPG) and volume into total gravity points (Total sugar extraction)
-    let totalGravityPoints (potential:float<ppg>) (vol : float<usGal>) =  
-        (float potential * float vol) * 1.0<gp>
+    ///Converts a points per gal (gp / usGal) and volume into total gravity points in that volume
+    let totalGravityPoints (potential:float<gp / usGal>) (vol : float<usGal>) =  
+        potential * vol
 
 (**
-The above example highlights a few things.
 
-1. We have defined further units of measure to fully restrict the functions input and output.
-2. In this case, it was more simple to cast the units to plain floats to do the calculation.
+As you can see, this function is declared with explicit type annotations specifying the dimensions of the parameters.  
+The FSharp compiler will now stop you from attempting to use this function with either dimensionless floats, or floats with the the wrong dimension.
+
+Consider the following example where we attempt to call the function with dimensionless values:
+
+*)
+
+    let totalGravPoints = totalGravityPoints 240.0 5
+
+(**
+Attempting to compile this line of code produces the following error (of many)
 
 
-The reason for casting to float here is this.  
+    [lang=output]
+    error FS0001: This expression was expected to have type
+    float<gp/usGal>    
+      but here has type
+    float 
+
+*)
+
+
+(**
+
+A few things need to be mentioned when working with `UoM`.
+
+1. You can declare a unit of measure to be made up of other units of measure.
+
+2. You can interchange UoM as long as they are equivalent. For example we could pass a ppg into the above function.
+
+3. In order to remove units from a measure you must either calculate them out or cast to float (effectively removing them).
+
+4. You can use generics with units of measure.
+
+Lets look at these points in more detail while staying in our domain of brewing.
+
+##Composing measures from measures
+
+    [<Measure>] type ppg = gp / usGal
+
+##Units of measure equivalence
+
+##Converting to and from units of measure
+
 PPG is a value representing Gravity Points (gp - Also in the example) per lb of grain, per US Gallon of extract. 
 When we multiply this by the volume we are saying that, for the given volume of extract with the given PPG, we would have the resulting value as total `gravity points`.
 
@@ -70,4 +115,8 @@ After all, the compiler has already done its job and prevented any incorrect uni
 Finally we multiply by `1.0<gp>`. This simply converts the output of the function to a `float<gp>`. Just what we need.
 
 Note: You can also use LanguagePrimitives.FloatWithMeasure to return a float with a given measure. This measure can either be specified explicitly or inferred by the type system.
+
+
+
+
 *)
