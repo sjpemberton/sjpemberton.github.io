5ec9cdf07f6a6b12364740ebe1551ffbd8d4af4e
Steve Pemberton <sjpemberton09@gmail.com>
2015-07-16 00:46:12 +0100

    Extra series support added

--- fsharp-posts/2015-04-22-FSharp-WPF-Part-2.fsx
+++ fsharp-posts/2015-04-22-FSharp-WPF-Part-2.fsx
@@ -620,7 +620,6 @@ date: 11/05/2015
 comments: true
 tags: ["fsharp","WPF"]
 catagories: ["guides","examples"]
-series: F# and WPF
 seriesId: FSharpWPF
 series-post-number: 2
 meta: Exploring the use of FSharp and WPF to create applications
